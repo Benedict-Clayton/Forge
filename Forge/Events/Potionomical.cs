e804e634f01fddd6bedb69dd2eb783d86ac3798b
Benedict Clayton <100662252+Benedict-Clayton@users.noreply.github.com>
2026-09-21 23:28:50 +1000

    WIP

---
+++ Forge/Events/Potionomical.cs
@@ -0,0 +1,106 @@
+using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Gold;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.PotionPools;
+using MegaCrit.Sts2.Core.Models.Potions;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace Forge;
+
+public sealed class Potionomical : CustomEventModel
+{
+    private PotionModel _potionOption;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new StringVar("PotionName", "")
+    };
+
+    public override bool IsAllowed(IRunState runState)
+    {
+        return runState.Players.All<Player>((Func<Player, bool>)(player => player.Potions.Any<PotionModel>()));
+    }
+
+    public override void CalculateVars()
+    {
+        _potionOption = Owner.Potions.Any()
+            ? Rng.NextItem(Owner.Potions)
+            : null;
+
+        if (_potionOption != null)
+            ((StringVar)DynamicVars["PotionName"]).StringValue = _potionOption.Title.GetFormattedText();
+        if (_cardOption != null)
+            ((StringVar)DynamicVars["CardName"]).StringValue = _cardOption.Title;
+    }
+
+
+    protected override Task BeforeEventStarted(bool isPreFinished)
+    {
+        Owner.CanUseOrRemovePotions = false;
+        return Task.CompletedTask;
+    }
+
+    protected override void OnEventFinished()
+    {
+        Owner.CanUseOrRemovePotions = true;
+    }
+
+    protected override IReadOnlyList<EventOption> GenerateInitialOptions()
+    {
+        var options = new List<EventOption>();
+
+        var title = L10NLookup($"{Id.Entry}.pages.INITIAL.options.GIVE_POTION.title");
+        var desc = L10NLookup($"{Id.Entry}.pages.INITIAL.options.GIVE_POTION.description");
+        options.Add(new EventOption(this,
+            async () => await GivePotion(_potionOption),
+            title, desc,
+            $"{Id.Entry}.pages.INITIAL.options.GIVE_POTION",
+            _potionOption.HoverTips).ThatHasDynamicTitle());
+
+        return options;
+    }
+
+    private async Task Experiment(PotionModel potion)
+    {
+        await PotionCmd.Discard(potion);
+        var rewards = new List<Reward>(choice);
+        // Gain a Colorless card reward.
+        for (int i = 0; i < 3; ++i)
+        {
+            rewards.Add(new CardReward(
+                CardCreationOptions.ForNonCombatWithDefaultOdds(
+                    new[] { (CardPoolModel)ModelDb.CardPool<ColorlessCardPool>() }),
+                3, Owner));
+        }
+
+        await RewardsCmd.OfferCustom(Owner, rewards);
+
+        SetEventFinished("EXPERIMENT");
+    }
+
+    private async Task BrewIt()
+    {
+        await RewardsCmd.OfferCustom(
+            Owner!,
+            new List<Reward>
+            {
+                new PotionReward(
+                    ModelDb.Potion<FoulPotion>().ToMutable(),
+                    Owner!)
+            });
+
+        SetEventFinished(PageDescription("BREW_IT"));
+    }
+}
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Art I did not draw that's not from STS1:
 All power icons come from Deathknell - https://store.steampowered.com/app/5105140/Death_Knell/ 
 
 Sputtering Furnace Event - https://www.artstation.com/artwork/d06PoK?album_id=6280492
+Gacha machine from Relic Gacha Event - https://x.com/jayjaynft4/status/2096524964656763179
